a22297d4d6642eaa0ee564fb8c7cfc425a0c2c1f
Neil Coutinho <njcoutinho@gmail.com>
2020-10-20 16:26:08 +0530

    Populate <video> tag with camera stream

--- pixelated-face.js
+++ pixelated-face.js
@@ -10,3 +10,18 @@ const faceDetector = new FaceDetector();
 
 console.log({webcam, videoCanvas, vctx, faceCanvas, fctx, faceDetector});
 
+
+async function populateVideo() {
+   let stream =  await navigator.mediaDevices.getUserMedia({
+       video: { width: 640, height: 480}
+   });
+
+   webcam.srcObject = stream;
+
+   webcam.play();
+
+   console.log(stream);
+}
+
+
+console.log(populateVideo());
