1799ab9f51758169f85946b416cf4512b2abe8e3
Neil Coutinho <njcoutinho@gmail.com>
2020-10-21 16:25:01 +0530

    Added a Readme.md + changed profile photo

---
+++ README.md
@@ -0,0 +1,6 @@
+# FACE DETECTION AND CENSORSHIP
+
+
+## Development server
+
+Run `npm install` to install dependencies followed by `npm start` to run server (parcel)
--- pixelated-face.js
+++ pixelated-face.js
@@ -6,14 +6,21 @@ const vctx = videoCanvas.getContext("2d");
 const faceCanvas = document.querySelector(".face");
 const fctx = faceCanvas.getContext("2d");
 
+const profilePhoto = document.getElementById("profilePhoto");
+
 const fd = new window.FaceDetector();
+
+if (window.FaceDetector == undefined) {
+    console.error('Face Detection not supported');
+    return;
+  }
 
 console.log({webcam, videoCanvas, vctx, faceCanvas, fctx, fd});
 
 
 async function populateVideo() {
    let stream =  await navigator.mediaDevices.getUserMedia({
-       video: { width: 640, height: 480}
+       video: { width: 1280, height: 720}
    });
 
    webcam.srcObject = stream;
@@ -39,9 +46,11 @@ async function populateVideo() {
 async function detect() {
     
     const faces = await fd.detect(webcam);
-    const profilePhoto = document.getElementById("profilePhoto");
+   
     const profileFace = await fd.detect(profilePhoto);
-    console.log({faces, profileFace})
+    console.log({faces, profileFace});
+
+   //requestAnimationFrame(detect);
 }
 
 populateVideo().then(detect)
